2b900f49aea6fdcbdfa06de0399e5382f08d9947
shakedba03 <shakedba03@gmail.com>
2020-12-25 10:28:36 +0200

    signup route

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 def index():
 	return render_template("index.html")
 
+@app.route('/signup')
+def signup():
+	return render_template("signup.html")
 
 
 if __name__ == '__main__':
