1d25c5e1cf004bdd981bc27a9b5eeef5baed0e78
shakedba03 <shakedba03@gmail.com>
2021-01-17 17:15:58 +0200

    projects screen

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template("projects.html")
 
 @app.route('/signup')
 def signup():
